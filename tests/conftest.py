59edf99f0b377415debbbd9779ca2942b91a9518
Evgeniy_Borovoy <borovoyevgen@gmail.com>
2025-10-20 18:05:00 +0200

    change user agent

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,7 +29,7 @@ def browser():
 def page(browser):
     context = browser.new_context(
         viewport={'width': 1920, 'height': 1080},
-        user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36',
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         base_url=Config.BASE_URL
     )
     page = context.new_page()
